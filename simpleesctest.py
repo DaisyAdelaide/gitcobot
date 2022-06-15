51fd331cabdba4acae199d8ce2a4a74a25d78419
DaisyAdelaide <daisybrown4444@gmail.com>
2022-06-15 14:37:54 -0700

    working to set motor at fixed speed for fixed time

---
+++ simpleesctest.py
@@ -0,0 +1,23 @@
+from time import sleep
+import pigpio
+import RPi.GPIO as GPIO
+
+pi = pigpio.pi()
+
+
+ESC_GPIO = 4
+print('start')
+
+pi.set_servo_pulsewidth(ESC_GPIO, 2200)
+sleep(4)
+pi.set_servo_pulsewidth(ESC_GPIO, 900)
+sleep(4)
+
+print('ready')
+
+speed = 1100
+pi.set_servo_pulsewidth(ESC_GPIO, speed)
+sleep(5)
+
+pi.set_servo_pulsewidth(ESC_GPIO, 0)
+pi.stop()
