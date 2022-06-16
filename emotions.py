d6588d730665937afabb91b223e498efa135d96e
DaisyAdelaide <daisybrown4444@gmail.com>
2022-06-16 12:43:55 -0700

    working with motor

--- emotions.py
+++ emotions.py
@@ -4,8 +4,10 @@ import csv
 import numpy
 import gpiozero
 import time
-from guizero import App,Picture, PushButton
-from PIL import Image
+from guizero import App, Picture, PushButton, Text
+from time import sleep
+import pigpio
+import RPi.GPIO as GPIO
 
 testing = []
 actions = []
@@ -19,11 +21,22 @@ f = open("file2.csv", "w")
 f.truncate()
 f.close()
 
+ESC_GPIO = 4
+pi = pigpio.pi()
+
+def arm():
+    pi.set_servo_pulsewidth(ESC_GPIO, 2500)
+    sleep(4)
+    pi.set_servo_pulsewidth(ESC_GPIO, 900)
+    sleep(4)
+    pi.set_servo_pulsewidth(ESC_GPIO, 500)
+    
 def listen1():
     with sr.Microphone(device_index = 1) as source:
         r = sr.Recognizer()
         r.adjust_for_ambient_noise(source)
         print("Say Something")
+        led3.on()
         audio = r.listen(source)
         print("got it")
     return audio
@@ -32,6 +45,8 @@ def voice(audio1):
     r = sr.Recognizer()
     text1 = r.recognize_google(audio1)
     print ("you said: " + text1)
+    heard = ("I heard... " + text1)
+    text = Text(app, text = heard, grid = [1,0])
     with open ("file1.csv","w",encoding='UTF8') as file:
         writer = csv.writer(file)
         writer.writerow(['command'])
@@ -43,6 +58,7 @@ def write_file_2():
     data3 = ['turn on red', 'you said red!']
     data4 = ['turn on blue','blue is my favourite']
     data5 = ['turn on Green','Green is nice']
+    data6 = ['start driving','drive!']
     
     with open('file2.csv','w',encoding='UTF8') as f:
         writer = csv.writer(f)
@@ -51,6 +67,7 @@ def write_file_2():
         writer.writerow(data3)
         writer.writerow(data4)
         writer.writerow(data5)
+        writer.writerow(data6)
   
 def print_reply(testing,actions,axis=1):
     index = 0
@@ -62,40 +79,46 @@ def print_reply(testing,actions,axis=1):
                 if index == 0:
                     led1.on()
                     picture = Picture(app, image = 'yellowcheeks.gif',grid = [0,0])
-                    time.sleep(2)
                 if index == 1:
                     led2.on()                    
                     picture = Picture(app, image = 'redbrows.gif',grid = [0,0])
-                    time.sleep(2)
                 if index == 2:
                     led3.on()
                     picture = Picture(app, image = 'bluenose.gif',grid = [0,0])
-                    time.sleep(2)
                 if index == 3:
                     led4.on()
                     picture = Picture(app, image = 'greeneyes.gif',grid = [0,0])
-                    time.sleep(2)
-               
+                if index == 4:
+                    drive()
             index = index + 1
         index = 0
+    
+def drive():
+    speed = 1100
+    pi.set_servo_pulsewidth(ESC_GPIO, speed)
+    sleep(10)
+    pi.set_servo_pulsewidth(ESC_GPIO, 500)
+    pi.stop()
+    picture = Picture(app, image = 'happy.gif',grid = [0,0])    
 
-def run():
+def record():
+    write_file_2()
     
     audio = listen1()
-
     voice(audio)
-
-    write_file_2()
-
+    
     testing = pd.read_csv('file1.csv')
     actions = pd.read_csv('file2.csv')
-
     print_reply(testing,actions)
-    
-    
-app = App(title = 'Cobot', width = 800, height = 800, layout = 'grid')
-picture = Picture(app, image = 'normal.gif', grid = [0,0])
+    led3.off()
 
-button = PushButton(app, command = run, grid = [0,1])
+#if 1st running since motors were powered
+#arm()
+    
+app = App(title = 'Cobot', width = 850, height = 700, layout = 'grid')
+
+picture = Picture(app, image = 'normal.gif', grid = [0,0])
+record = PushButton(app, command = record, text = 'record', grid = [0,1])
+
 
 app.display()
