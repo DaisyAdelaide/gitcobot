e8911d7f85ba1c992ab633db6e68f3fc48608560
DaisyAdelaide <daisybrown4444@gmail.com>
2022-06-21 09:08:15 -0700

    general update

--- simpleesctest.py
+++ simpleesctest.py
@@ -12,12 +12,13 @@ pi.set_servo_pulsewidth(ESC_GPIO, 2200)
 sleep(4)
 pi.set_servo_pulsewidth(ESC_GPIO, 900)
 sleep(4)
+pi.set_servo_pulsewidth(ESC_GPIO, 500)
 
 print('ready')
 sleep(2)
 speed = 1100
 pi.set_servo_pulsewidth(ESC_GPIO, speed)
-sleep(10)
+sleep(5)
+
 
 pi.set_servo_pulsewidth(ESC_GPIO, 500)
-sleep(4)
