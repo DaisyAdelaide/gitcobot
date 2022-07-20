341fc695b52a1dcecd1dd40e9063ac9d9da0dbda
DaisyAdelaide <daisybrown4444@gmail.com>
2022-07-20 20:42:32 +0100

    Update blink.py

--- version1.0/blink.py
+++ version1.0/blink.py
@@ -95,7 +95,7 @@ def chatbot():
         textRect.center = (400, 450)
         
         #screen.fill(black)
-        pygame.draw.rect(screen, black, pygame.Rect(0, 0, 400, 450))
+        pygame.draw.rect(screen, black, pygame.Rect(400, 0, 200, 800))
         screen.blit(textSurface, textRect)
         pygame.display.update()
         clock.tick(20)
