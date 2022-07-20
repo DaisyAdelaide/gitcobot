a1fd230e05d85fb38b1b0b6cf7b15e2edaed353e
DaisyAdelaide <daisybrown4444@gmail.com>
2022-07-20 19:03:59 +0100

    Update getresponse.py

--- version1.0/getresponse.py
+++ version1.0/getresponse.py
@@ -16,7 +16,8 @@ def get_intent(text):
 	input_phrase = {
 					'Greeting': r'hi\s',
 					'Greeting': r'hello\s',
-					'DescribeSelf': r'what\s',
+					'DescribeSelf': r'what is\s',
+					'How': r'how\s',
 					'no_match_intent': r''
 
 					}
@@ -32,6 +33,9 @@ def get_intent(text):
 			return Greet()
 		if found_match and intent == 'DescribeSelf':
 			return DescribeSelf()
+		if found_match and intent == 'How':
+			return How()
+
 
 def no_match_intent():
 	responses = ('Please tell me more.','Tell me more!','“Why do you say that?','I see. Can you elaborate?','Interesting. Can you tell me more?','I see. How do you think?','Why?','How do you think I feel when you say that?')
@@ -43,6 +47,10 @@ def Greet():
 
 def DescribeSelf():
 	responses = ('I am the Cobot', 'I am a robot')
+	return random.choice(responses)
+
+def How():
+	responses = ('I am great', 'Doing well and you?', 'Thanks for asking! Good!')
 	return random.choice(responses)
 
 #get_response('what is your name ')
