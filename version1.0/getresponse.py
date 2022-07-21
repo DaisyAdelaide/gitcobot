8a03eb545efe7c13d859db92aa01ceff47415e3e
DaisyAdelaide <daisybrown4444@gmail.com>
2022-07-21 23:01:34 +0100

    Update getresponse.py

--- version1.0/getresponse.py
+++ version1.0/getresponse.py
@@ -19,7 +19,7 @@ def get_response(text, name_index):
 def get_intent(text):
 	input_phrase = {
 					'name': r'.*\s*Hi.*',
-					'name': r'.*\s*hello.*',
+					'Greeting': r'.*\s*hello.*',
 					'DescribeSelf': r'.*\s*name.*',
 					'How': r'how\s',
 					'Animal':  r'.*\s*animal.*',
@@ -38,7 +38,7 @@ def get_intent(text):
 		if found_match and intent == 'no_match_intent':
 			return no_match_intent()
 		if found_match and intent == 'Greeting':
-			return Greet()
+			return name()
 		if found_match and intent == 'DescribeSelf':
 			return DescribeSelf()
 		if found_match and intent == 'How':
