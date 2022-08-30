46e0d8f471dd25cd44ae3306770c946cea38d24c
DaisyAdelaide <daisybrown4444@gmail.com>
2022-08-30 11:48:10 -0700

    Update blink.py

--- version1.0/blink.py
+++ version1.0/blink.py
@@ -220,10 +220,7 @@ speed = '0'
 while True:
     if ser.in_waiting > 0:
         line = ser.readline().decode('latin-1').rstrip()
-        try:
-            speed = line
-        except:
-            speed = '0'
+        speed = line
         with open ("SpeedData.csv","a",encoding='UTF8') as file:
             writer = csv.writer(file)
             writer.writerow([line])
