a50d255d817d9319f863b2ee47032168eff51b77
DaisyAdelaide <daisybrown4444@gmail.com>
2022-09-06 15:54:13 -0700

    Update blink.py

--- version1.0/blink.py
+++ version1.0/blink.py
@@ -317,9 +317,9 @@ def maths_game():
 
         
         
-        screen.blit(dog, (250,150))
-        screen.blit(cat, (350,100))
-        screen.blit(snail, (450,170))
+        screen.blit(dog, (250,250))
+        screen.blit(cat, (350,200))
+        screen.blit(snail, (450,270))
         screen.blit(podium,(250, 250))
 
         pygame.display.update()
