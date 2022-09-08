2b68a1c32bb66065c57039ee3c2b7d79008d2527
DaisyAdelaide <daisybrown4444@gmail.com>
2022-09-08 16:28:07 -0700

    Update blink.py

--- version1.0/blink.py
+++ version1.0/blink.py
@@ -290,6 +290,7 @@ def maths_game():
                             if character_chosen == animals[i]:
                                 scores[i] += points 
                             i += 1
+            summ()
 
         load_animals()
 
