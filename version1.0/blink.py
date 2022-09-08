acaa1ede3a190c0545e15abc8cb67bbaa352c123
DaisyAdelaide <daisybrown4444@gmail.com>
2022-09-08 10:04:14 -0700

    Update blink.py

--- version1.0/blink.py
+++ version1.0/blink.py
@@ -365,8 +365,8 @@ def maths_game():
 
 
         if draw1 == 1:
-            first_place = pygame.transform.scale(first_place,(160,160))
-            screen.blit(first_place, (50, 350))
+            first_place = pygame.transform.scale(first_place,(180,180))
+            screen.blit(first_place, (50, 300))
         if draw2 == 1:
             second_place = pygame.transform.scale(second_place,(100,100))
             screen.blit(second_place, (350,380))
