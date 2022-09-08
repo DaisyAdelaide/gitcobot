7e8c6b71e305d5eb9c1c7a71f91701d1b4f535d8
DaisyAdelaide <daisybrown4444@gmail.com>
2022-09-08 16:07:55 -0700

    Update blink.py

--- version1.0/blink.py
+++ version1.0/blink.py
@@ -259,74 +259,9 @@ def maths_game():
                 screen.blit(textSurface, textRect)
                 pygame.display.update()
                 clock.tick(20)
-                time.sleep(1)
+                time.sleep(2)
 
-                if right == 1:
-                    select_character()
-
-                    audio = functions.listen1()
-                    character_chosen = functions.voice(audio)
-
-                    character_chosen = str(character_chosen)
-                    
-                    if character_chosen == 'I didnt catch that!':
-                        wrong_sound = mixer.Sound('wrong.wav')
-                        wrong_sound.play()
-
-                    else:
-                        x = 0
-                        for animal in animals:
-                            if character_chosen == animal:
-                                i = 0
-                                for index in scores:
-                                    if character_chosen == animals[i]:
-                                        scores[i] += points 
-                                    i += 1
-                            x = 1
-
-                        if x == 0:
-                            character_chosen = lev_animal.find_match(character_chosen)
-                            for animal in animals:
-                                if character_chosen == animal:
-                                    i = 0
-                                    for index in scores:
-                                        if character_chosen == animals[i]:
-                                            scores[i] += points 
-                                        i += 1
-
-                    load_animals()
-
-                    dict1 = {animal_images[i]:scores[i]for i in range(len(animal_images)) if scores[i]>0 }
-
-                    dict1 = dict(sorted(dict1.items(), key=operator.itemgetter(1)))
-
-                    list1 = list(dict1.keys())
-                    list1.reverse()
-
-                    x = 0
-                    if len(list1) > 0:
-                        for animal in list1:
-                            lookup = animal
-                            animal = pygame.transform.scale(animal,(130-x*30,130-x*30))
-                            screen.blit(animal, (30 + x*130,350+x*30))
-
-                            font = pygame.font.Font('freesansbold.ttf', 50)
-                            textSurface3 = font.render('{}'.format(dict1[lookup]), True, white, blue)
-                            textRect3 = textSurface3.get_rect()
-                            textRect3.center = (30 + x*130,300+x*30)
-                            screen.blit(textSurface3, textRect3)
-
-                            x += 1
-
-                    screen.fill(blue)
-                    color_picked = blue
-                    textSurface = font.render(problem, True, yellow, blue)
-                    textRect = textSurface.get_rect()
-                    textRect.center = (400, 150)
-
-                    screen.blit(textSurface, textRect)
-                    pygame.display.update()
-                    clock.tick(20)
+                
 
 
 def select_character():
