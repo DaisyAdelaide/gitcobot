64da487a3acc36d6871a5c851724a8114cd54f33
DaisyAdelaide <daisybrown4444@gmail.com>
2022-09-09 14:43:48 -0700

    Update playsound.py

--- version1.0/playsound.py
+++ version1.0/playsound.py
@@ -4,6 +4,7 @@ import pyttsx3, time
 engine = pyttsx3.init() 
 
 engine.setProperty('rate', 125)
+engine.setProperty('voice', voices[1].id)
 engine.say("Hello I am the cobot") 
 engine.runAndWait()
 
