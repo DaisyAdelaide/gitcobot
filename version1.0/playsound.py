c0cfbdd4b563a91907594bc9c8aabc3801d05e47
DaisyAdelaide <daisybrown4444@gmail.com>
2022-09-09 16:03:18 -0700

    Update playsound.py

--- version1.0/playsound.py
+++ version1.0/playsound.py
@@ -3,8 +3,6 @@
 import pyttsx3
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
-voiceFemales = filter(lambda v: v.gender == 'VoiceGenderFemale', voices)
-for v in voiceFemales:
-    engine.setProperty('voice', v.id)
-    engine.say('Hello world from ' + v.name)
-    engine.runAndWait()
+engine.setProperty('rate', 196)
+engine.setProperty('volume', 2.7)
+engine.setProperty('voice', voices[1].id)
