0e0ca5b1dc9b0a99e42eb7d851b26142833e89ef
DaisyAdelaide <daisybrown4444@gmail.com>
2022-09-09 16:10:11 -0700

    Update playsound.py

--- version1.0/playsound.py
+++ version1.0/playsound.py
@@ -3,8 +3,10 @@
 import pyttsx3
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
-engine.setProperty('rate', 196)
-engine.setProperty('volume', 2.7)
-engine.setProperty('voice', voices[1].id)
-engine.say('Hello world from ')
+voice = engine.getProperty('voices') #get the available voices
+    # eng.setProperty('voice', voice[0].id) #set the voice to index 0 for male voice
+engine.setProperty('voice', voice[1].id)
+engine.say('hi there my name is daisy')
+for voice in voices:
+	print(voice)
 engine.runAndWait()
