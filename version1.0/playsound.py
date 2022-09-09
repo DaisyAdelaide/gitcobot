d5fecfafff89004a97df0206059ef16ad3d4fda0
DaisyAdelaide <daisybrown4444@gmail.com>
2022-09-09 14:54:07 -0700

    Update playsound.py

--- version1.0/playsound.py
+++ version1.0/playsound.py
@@ -4,6 +4,6 @@ import pyttsx3, time
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 for voice in voices:
-   print(engine.setProperty('voice', voice.id))
+   print(voice)
    #engine.say('The quick brown fox jumped over the lazy dog.')
 engine.runAndWait()
