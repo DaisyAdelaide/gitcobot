d5821ae5acaee5c7958d165847c09da4db9d06a6
DaisyAdelaide <daisybrown4444@gmail.com>
2022-09-29 19:27:08 -0700

    Update google.py

--- version1.0/google.py
+++ version1.0/google.py
@@ -6,18 +6,19 @@ headers = {
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36'
 }
 
-html = requests.get('https://www.google.com/search?q="where is dublin', headers=headers)
+html = requests.get('https://www.google.com/search?q="where is google', headers=headers)
 soup = BeautifulSoup(html.text, 'html.parser')
 
 answer = 'i dont know'
 
-try:
-	answer = soup.select_one('.SPZz6b').text
 
-except:
-	answer = soup.select_one('.t2b5Cf').text
+#try:
+#answer = soup.select_one('.SPZz6b').text
 
+#except:
+	#answer = soup.select_one('.t2b5Cf').text
 
+answer = soup.find('div',class_='SPZz6b').find_next('span').text
 
 
 print(answer)
@@ -26,3 +27,4 @@ print(answer)
 
 
 # This kinda works !!!! Fix it up a bit
+# Try is not working maybe if statements ? 
