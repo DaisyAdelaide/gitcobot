3d73cd5b2c5d56db8a53a85da364aa45b2497605
DaisyAdelaide <daisybrown4444@gmail.com>
2022-10-06 18:07:55 -0700

    update

--- version1.0/google.py
+++ version1.0/google.py
@@ -6,7 +6,7 @@ headers = {
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36'
 }
 
-html = requests.get('https://www.google.com/search?q="how do i solve a rubiks cube', headers=headers)
+html = requests.get('https://www.google.com/search?q="do aliens exist', headers=headers)
 soup = BeautifulSoup(html.text, 'html.parser')
 
 
