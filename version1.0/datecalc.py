7f4c8c0c82f2ca9c77ecaffbaed7a901ed9d62eb
DaisyAdelaide <daisybrown4444@gmail.com>
2022-10-27 10:46:49 -0700

    update

--- version1.0/datecalc.py
+++
@@ -1,5 +0,0 @@
-from datetime import datetime
-then = datetime(2022,6,5)        # Random date in the past
-now  = datetime.now()                         # Now
-duration = now - then 
-print (duration)
