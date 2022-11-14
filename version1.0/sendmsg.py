3e88f47fde840f17637e47cf53665d76d9650cf0
DaisyAdelaide <daisybrown4444@gmail.com>
2022-11-14 15:43:02 -0800

    Update sendmsg.py

--- version1.0/sendmsg.py
+++ version1.0/sendmsg.py
@@ -5,7 +5,7 @@ TWILIO_AUTH_TOKEN = 'f673fb607500e2498f556812cd6ccf09' # replace with your Auth 
 TWILIO_PHONE_SENDER = "+15097785509" # replace with the phone number you registered in twilio
 TWILIO_PHONE_RECIPIENT = "+12365917334" # replace with your phone number
 
-    """Sends an SMS text alert."""
+"""Sends an SMS text alert."""
 client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 message = client.messages.create(
     to=TWILIO_PHONE_RECIPIENT,
