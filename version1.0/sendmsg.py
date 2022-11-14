90d7b08a238b4e048580a667a04df1e78c9c76a4
DaisyAdelaide <daisybrown4444@gmail.com>
2022-11-14 15:46:29 -0800

    Update sendmsg.py

--- version1.0/sendmsg.py
+++ version1.0/sendmsg.py
@@ -1,14 +1,14 @@
+import os
 from twilio.rest import Client
 
-TWILIO_ACCOUNT_SID = 'AC8ce5d0f9b9f6c771979323a81a3f66fb' # replace with your Account SID
-TWILIO_AUTH_TOKEN = 'f673fb607500e2498f556812cd6ccf09' # replace with your Auth Token
-TWILIO_PHONE_SENDER = "+15097785509" # replace with the phone number you registered in twilio
-TWILIO_PHONE_RECIPIENT = "+12365917334" # replace with your phone number
+account_sid = os.environ['AC8ce5d0f9b9f6c771979323a81a3f66fb']
+auth_token = os.environ['6f3528ebbd747c9a2c292a7aebe29cb5']
+client = Client(account_sid, auth_token)
 
-"""Sends an SMS text alert."""
-client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 message = client.messages.create(
-    to=TWILIO_PHONE_RECIPIENT,
-    from_=TWILIO_PHONE_SENDER,
-    body="hi from clover")
+  body='Hi there',
+  from_='+15097785509',
+  to='+12365917334'
+)
+
 print(message.sid)
