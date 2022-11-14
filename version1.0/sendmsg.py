d10272e3622e141ee05adce408c51e0f7a049f4f
DaisyAdelaide <daisybrown4444@gmail.com>
2022-11-14 15:48:49 -0800

    Update sendmsg.py

--- version1.0/sendmsg.py
+++ version1.0/sendmsg.py
@@ -1,8 +1,8 @@
 import os
 from twilio.rest import Client
 
-account_sid = os.environ['AC8ce5d0f9b9f6c771979323a81a3f66fb']
-auth_token = os.environ['6f3528ebbd747c9a2c292a7aebe29cb5']
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
