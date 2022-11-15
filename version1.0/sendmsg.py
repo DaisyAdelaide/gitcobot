b62176c633df0beea88170a46d46a48e0452322f
DaisyAdelaide <daisybrown4444@gmail.com>
2022-11-14 16:32:19 -0800

    Update sendmsg.py

--- version1.0/sendmsg.py
+++ version1.0/sendmsg.py
@@ -1,7 +1,9 @@
+import os
 from twilio.rest import Client
 
-TWILIO_ACCOUNT_SID = 'AC8ce5d0f9b9f6c771979323a81a3f66fb' # replace with your Account SID
-TWILIO_AUTH_TOKEN = '6f3528ebbd747c9a2c292a7aebe29cb5' # replace with your Auth Token
+
+TWILIO_ACCOUNT_SID = os.environ['TWILIO_ACCOUNT_SID'] # replace with your Account SID
+TWILIO_AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN'] # replace with your Auth Token
 TWILIO_PHONE_SENDER = "+15097785509" # replace with the phone number you registered in twilio
 TWILIO_PHONE_RECIPIENT = "+12365917334" # replace with your phone number
 
