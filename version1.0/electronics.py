5ad0457840c40c08d1e73f5cbae2b07932bbea57
DaisyAdelaide <daisybrown4444@gmail.com>
2023-01-24 13:19:05 -0800

    electronics game

--- version1.0/electronics.py
+++ version1.0/electronics.py
@@ -13,8 +13,16 @@ def load_symbols():
 	resistor = pygame.transform.scale(resistor,(100,100))
 	inductor = pygame.image.load('inductor.png').convert_alpha()
 	inductor = pygame.transform.scale(inductor,(100,100))
+	battery = pygame.image.load('battery.png').convert_alpha()
+	battery = pygame.transform.scale(battery,(100,100))
+	wire = pygame.image.load('wire.png').convert_alpha()
+	wire = pygame.transform.scale(wire,(100,100))
+	switch = pygame.image.load('switch.png').convert_alpha()
+	switch = pygame.transform.scale(switch,(100,100))
+	led = pygame.image.load('led.png').convert_alpha()
+	led = pygame.transform.scale(led,(100,100))
 
-	electronic_images = [dc, ac, resistor, inductor]
+	electronic_images = [dc, ac, resistor, inductor, battery, wire, switch, led]
 
 
 pygame.init()
@@ -27,18 +35,31 @@ question = 0
 
 white = (255, 255, 255)
 red = (255, 51, 58)
+button1Color = (4, 99, 7)
+button2Color = (106, 207, 101)
+button3Color = (61, 236, 85)
+
+
 
 font = pygame.font.Font('freesansbold.ttf', 100)
 gui_font = pygame.font.Font(None,30)
 
-electronics = ['DC source', 'AC source', 'Resistor', 'Inductor']
+electronics = ['DC source', 'AC source', 'Resistor', 'Inductor', 'Battery', 'Wire', 'Switch','LED']
+
+buttonNumber = 0
+
+def buttonColors():
+	global button1Color, button2Color, button3Color
+	button1Color = (4, 99, 7)
+	button2Color = (106, 207, 101)
+	button3Color = (61, 236, 85)
 
 load_symbols()
 
 class Button:
-    def __init__(self, text, width, height, pos):
+    def __init__(self, text, width, height, pos, color):
         self.top_rect = pygame.Rect(pos,(width,height))
-        self.top_color = (100,120,100)
+        self.top_color = color
         self.text_surf = gui_font.render(text, 0,(210,255,100))
         self.text_rect = self.text_surf.get_rect(center = self.top_rect.center)
         
@@ -56,10 +77,11 @@ class Button:
         
         return action
 
-buttonOne = Button('', 200, 40, (200,400))
-buttonTwo = Button('', 200, 40, (400,400))
-
 while True:
+
+	buttonOne = Button('', 275, 480, (0,0), button1Color)
+	buttonTwo = Button('', 250, 480, (275,0), button2Color)
+	buttonThree = Button('',275, 480, (525, 0), button3Color)
 
 	for event in pygame.event.get():
 	    if event.type == pygame.QUIT:
@@ -71,8 +93,8 @@ while True:
 	            sys.exit()
 
 	if question == 0:
-		picked = random.randint(0,3)
-		numbers = [0,1,2,3]
+		picked = random.randint(0,7)
+		numbers = [0,1,2,3,4,5,6,7]
 		numbers.remove(picked)
 		second = random.choice(numbers)
 		numbers.remove(second)
@@ -80,13 +102,19 @@ while True:
 		question += 1
 		all_choice = [picked, second, third]
 		random.shuffle(all_choice)
+		index = 0
+		for element in all_choice:
+			if element == picked:
+				buttonNumber = index
+			index += 1
 
-	textSurface = font.render(electronics[picked], True, red, white)
+
+	textSurface = font.render(electronics[picked], True, white, (9, 77, 28))
 	textRect = textSurface.get_rect()
 	textRect.center = (400, 100)
 
 
-	screen.fill((25, 130, 196))
+	screen.fill((9, 77, 28))
 
 	x = 150
 	for element in all_choice:
@@ -94,14 +122,27 @@ while True:
 		x += 200
 
 	if buttonOne.draw():
-	    question = 0
+		if buttonNumber == 0:
+			question = 0
+			buttonColors()
+		else:
+			button1Color = (255,0,0)
 	    
 	if buttonTwo.draw():
-	    question = 1
+	    if buttonNumber == 1:
+	        question = 0
+	        buttonColors()
+	    else:
+	    	button2Color = (255,0,0)
+
+	if buttonThree.draw():
+	    if buttonNumber == 2:
+	        question = 0
+	        buttonColors()
+	    else:
+	    	button3Color = (255,0,0)
 
 	screen.blit(textSurface, textRect)
 	pygame.display.update()
 	clock.tick(10)
 
-
-	#this is working to display images, check notebook for next steps. 
