fcdbe785140c5b885d3644a832a2c6547d0235a9
DaisyAdelaide <daisybrown4444@gmail.com>
2023-01-24 14:41:51 -0800

    Update electronics.py

--- version1.0/electronics.py
+++ version1.0/electronics.py
@@ -132,8 +132,8 @@ while True:
 
 	x = 100
 	for element in all_choice:
-		screen.blit(electronic_images[element], (0+x,300))
-		x += 200
+		screen.blit(electronic_images[element], (0+x,250))
+		x += 300
 
 	if buttonOne.draw():
 		ts = time.time()
